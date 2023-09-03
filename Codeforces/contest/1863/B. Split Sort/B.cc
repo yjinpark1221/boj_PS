afff880618c34b69f775a913ccc7c2385d9bd43c
yjinpark1221 <67498785+yjinpark1221@users.noreply.github.com>
2023-09-04 01:52:15 +0900

    [B - Pinely Round 2 (Div. 1 + Div. 2)] Title: Split Sort, Time: 30 ms, Memory: 400 KB

---
+++ Codeforces/contest/1863/B. Split Sort/B.cc
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <queue>
+#include <stack>
+#include <set>
+#include <cstring>
+#include <ctime>
+#include <list>
+#include <numeric> 
+#include <deque>
+#include <functional>
+#include <limits.h>
+#include <tuple>
+ 
+#define FOR(i, n) for (int i = 0; i < (n); ++i)
+#define FORV(iter, v) for (auto iter = v.begin(); iter != v.end(); ++iter)
+#define SWAP(a, b) {auto tmp = a;a = b;b = tmp;}
+#define x first
+#define y second
+#define MAX 2147483647
+#define MIN -2147483648
+#define MOD 1000000007
+ 
+typedef long long ll;
+typedef std::pair<int, int> pii;
+typedef std::pair<ll, ll> pll;
+typedef std::vector<int> vi;
+typedef std::vector<ll> vl;
+typedef std::pair<int, pii> piii;
+typedef std::pair<pii, pii> piiii;
+typedef std::vector<char> vc;
+ 
+typedef std::pair<ll, ll> point;
+typedef std::pair<ll, ll> vec;
+ 
+template<typename T>
+using priority_stack = std::priority_queue<T, std::vector<T>, std::greater<T>>;
+using namespace std;
+ 
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int t;
+    for (cin >> t; t--;) {
+        int n, cnt = 0;
+        vi v;
+        cin >> n;
+        v.resize(n);
+        FOR(i, n) {
+            int tmp;
+            cin >> tmp;
+            v[tmp - 1] = i;
+        }
+        FOR(i, n - 1) {
+            if (v[i] > v[i + 1]) {
+                ++cnt;
+            }
+        }
+        cout << cnt << "\n";
+    }
+    return 0;
+}
